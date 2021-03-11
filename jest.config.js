31ae6995119c399b3ba8b33aab377e86130c7622
Gustavo Varallo <gustavo.varallo@live.com>
2021-03-11 10:58:12 -0300

    chore: add files to covarage ignore

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.{ts,tsx}', '!**/*.d.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.{ts,tsx}',
+    '!<rootDir>/src/main/**/*',
+    '!<rootDir>/src/domain/models/index.ts',
+    '!<rootDir>/src/domain/usecases/index.ts',
+    '!<rootDir>/src/presentation/components/router/**/*',
+    '!**/*.d.ts'
+  ],
   coverageDirectory: 'coverage',
   testEnvironment: 'jsdom',
   transform: {
