c1f6fb0d841a4cc02395a15f15fb025bca5e9e41
Gustavo Varallo <gustavo.varallo@live.com>
2021-03-22 15:18:29 -0300

    test: ensure signup presents EmailInUseError on 403

--- src/main/test/cypress/integration/signup.spec.ts
+++ src/main/test/cypress/integration/signup.spec.ts
@@ -1,6 +1,11 @@
 import faker from 'faker'
 
-import { fillFormCorrectly, testInputStatus } from '../support/form-helper'
+import {
+  baseUrl,
+  fillFormCorrectly,
+  submit,
+  testInputStatus
+} from '../support/form-helper'
 
 describe('SignUp', () => {
   beforeEach(() => cy.visit('signup'))
@@ -35,5 +40,18 @@ describe('SignUp', () => {
     testInputStatus('passwordConfirmation', 'valid', 'Looks good', '✔')
     cy.getByTestId('submit').should('not.have.attr', 'disabled')
     cy.getByTestId('error-wrap').should('not.have.descendants')
+  })
+
+  it('Should present EmailInUseError on 403', () => {
+    cy.intercept('POST', /signup/, {
+      statusCode: 403,
+      body: {
+        error: faker.random.words()
+      }
+    })
+    fillFormCorrectly('email', 'password', 'name', 'passwordConfirmation')
+    submit()
+    cy.getByTestId('main-error').should('contain.text', 'Email already in use')
+    cy.url().should('eq', `${baseUrl}/signup`)
   })
 })
