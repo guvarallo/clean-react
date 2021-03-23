f716e792a55304564017e03140e057181b42fa40
Gustavo Varallo <gustavo.varallo@live.com>
2021-03-23 17:05:43 -0300

    refactor: move header to its own file

---
+++ src/presentation/components/header/header.tsx
@@ -0,0 +1,21 @@
+import React, { memo } from 'react'
+
+import Styles from './header-styles.scss'
+
+import { Logo } from '..'
+
+const Header: React.FC = () => {
+  return (
+    <header className={Styles.headerWrap}>
+      <div className={Styles.headerContent}>
+        <Logo />
+        <div className={Styles.logoutWrap}>
+          <span>Gustavo</span>
+          <a href='#'>Logout</a>
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default memo(Header)
